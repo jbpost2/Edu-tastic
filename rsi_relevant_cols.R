ae981d791ec53a8585d32d3f22484259a7339745
vseng <60186303+vseng@users.noreply.github.com>
2022-04-04 16:46:09 -0400

    fixed rsi so its just showing NC counties

--- rsi_relevant_cols.R
+++ rsi_relevant_cols.R
@@ -4,6 +4,7 @@ rsi <- read_csv("rsi_full.csv")
 
 rsi <- rsi %>%
          select(countyname,
+           state,
            county_pct_rural, 
            cluster_pct_rural_avg, 
            not_adjacent_urban,
@@ -11,8 +12,9 @@ rsi <- rsi %>%
            lowemp,
            perpov,
            perchldpov
-           ) %>%
-           mutate(countyname = str_sub(countyname, end = -11))
-    
-  
+           )%>%
+           mutate(countyname = str_sub(countyname, end = -11)) %>%
+           filter(state == "North Carolina" & duplicated(countyname == FALSE))
+
+
 
