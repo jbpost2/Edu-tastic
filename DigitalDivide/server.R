74d50def312effa47e2f37ccacca2872d5f43802
JBPOST2 <jbpost2@ncsu.edu>
2022-04-05 12:08:22 -0400

    added color click

--- DigitalDivide/server.R
+++ DigitalDivide/server.R
@@ -29,17 +29,36 @@ shinyServer(function(input, output, session) {
   
       #Create Scatter Plot
   output$edaPlot <- renderPlotly({
-    if (input$class){(
-      ggplotly(
-        ggplot(data = filter(digital, URBAN_RURAL == "Rural") , aes(x=!!rlang::sym(input$xvar), y=!!rlang::sym(input$yvar), size = Population, color = Population, text = paste("County:", County) )) + 
+    click_data <- event_data(event = "plotly_click", source = "select")
+    digital$key <- row.names(digital)
+    if (!is.null(click_data)) {
+      highlight <- digital[digital$key %in% click_data$key,]
+      print(click_data$key)
+    }
+    if (input$class){
+      if (!is.null(click_data)) {
+        p <-  ggplot(data = filter(digital, URBAN_RURAL == "Rural") , aes(x=!!rlang::sym(input$xvar), y=!!rlang::sym(input$yvar), size = Population, color = Population, text = paste("County:", County), key = key )) + 
           geom_point(alpha=0.7) + 
-          scale_size( name="Population"))
-    )}else{
-    
-    ggplotly(
-    ggplot(data = digital, aes(x=!!rlang::sym(input$xvar), y=!!rlang::sym(input$yvar), size = Population, color = URBAN_RURAL, text = paste("County:", County) )) + 
+          scale_size( name="Population") +
+          geom_point(data = highlight, aes(x=!!rlang::sym(input$xvar), y=!!rlang::sym(input$yvar)), color = "green")
+      } else {
+          p <-  ggplot(data = filter(digital, URBAN_RURAL == "Rural") , aes(x=!!rlang::sym(input$xvar), y=!!rlang::sym(input$yvar), size = Population, color = Population, text = paste("County:", County), key = key )) + 
+          geom_point(alpha=0.7) + 
+          scale_size( name="Population")
+      }
+    } else {
+       if (!is.null(click_data)) {
+    p <- ggplot(data = digital, aes(x=!!rlang::sym(input$xvar), y=!!rlang::sym(input$yvar), size = Population, color = URBAN_RURAL, text = paste("County:", County),key = key  )) +
+      geom_point(alpha=0.7) +
+      scale_size( name="Population") +
+         geom_point(data = highlight, aes(x=!!rlang::sym(input$xvar), y=!!rlang::sym(input$yvar)), color = "green")
+      } else {
+         p <- ggplot(data = digital, aes(x=!!rlang::sym(input$xvar), y=!!rlang::sym(input$yvar), size = Population, color = URBAN_RURAL, text = paste("County:", County),key = key)) + 
       geom_point(alpha=0.7) + 
-      scale_size( name="Population"))}
+      scale_size( name="Population")
+      }
+    }
+    ggplotly(p, source = "select")
   })
   
   output$table <- renderDataTable(digital,
